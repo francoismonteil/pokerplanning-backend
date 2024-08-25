158bc146a1f605efbed56fa9b8f542fd702e8bff
francoismonteil <francois.monteil@protonmail.com>
2024-08-25 11:15:15 +0200

    Initialisation du projet

---
+++ src/main/java/fr/pokerplanning/PokerplanningApplication.java
@@ -0,0 +1,13 @@
+package fr.pokerplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerplanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerplanningApplication.class, args);
+    }
+
+}
